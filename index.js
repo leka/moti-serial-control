adfbf62f49319b97218c087067ea6686421e17a9
Flavien Raynaud <raynaudfla@eisti.eu>
2014-07-23 16:15:33 +0200

    Better interface.

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ function error_callback(err, bytesWritten){
 }
 
 
-function sendData(data, port) {
+function sendData(data) {
 	var data_string = "";
 
 	data.map(function(item) {
 		data_string += String.fromCharCode(item);
 	});
 	
-	port.write(new Buffer(data_string, 'utf-8'), error_callback);
+	if (portType == 'USB')
+		myPort.write(data_string);
+	else if (portType == 'Bluetooth')
+		myBTPort.write(new Buffer(data_string, 'utf-8'), error_callback);
+	else
+		console.log("Error, cannot send data: not connected.");
 }
 
 
@@ -46,38 +51,9 @@ console.log("opening serial port: " + portName);	// print out the port you're li
 server.listen(8080);								// listen for incoming requests on the server
 console.log("Listening for new clients on port 8080");
 
-
-// open the serial port. Change the name to the name of your port, just like in Processing and Arduino:
-var myPort = new SerialPort(portName,
-	{
-		baudrate: 115200,
-		// look for return and newline at the end of each data packet:
-		parser: serialport.parsers.readline("\r\n")
-	}
-);
-
-
-var myBtPort = new (require('bluetooth-serial-port')).BluetoothSerialPort();
-myBtPort.on('found', function (address, name) {
-	console.log('Found: ' + address + ' with name ' + name);
-	if (name.indexOf('moti') != -1) {
-		myBtPort.findSerialPortChannel(address, function(channel) {
-			console.log('Found RFCOMM channel for serial port on ' + name + ': ' + channel);
-
-			console.log('Attempting to connect to moti...');
-
-			myBtPort.connect(address, channel, function(){
-				console.log('Connected. Receiving data...');
-			});
-		});
-
-		console.log("Closing port");
-		myBtPort.close();
-	}
-});
-
-myBtPort.inquire();
-
+var myPort;
+var myBTPort = new (require('bluetooth-serial-port')).BluetoothSerialPort();
+var portType = '';
 
 
 // respond to web GET requests with the index.html page:
@@ -94,22 +70,22 @@ io.sockets.on('connection', function (socket) {
 		switch (keyCode) {
 			case 37:
 				console.log('Left');
-				sendData(dataSpin(0, 255, 160), myBtPort);
+				sendData(dataSpin(0, 255, 160));
 				break;
 
 			case 38:
 				console.log('Forward');
-				sendData(dataGo(1, 255, 0), myBtPort);
+				sendData(dataGo(1, 255, 0));
 				break;
 
 			case 39:
 				console.log('Right');
-				sendData(dataSpin(1, 255, 160), myBtPort);
+				sendData(dataSpin(1, 255, 160));
 				break;
 
 			case 40:
 				console.log('Backward');
-				sendData(dataGo(0, 255, 0), myBtPort);
+				sendData(dataGo(0, 255, 0));
 				break;
 
 			default:
@@ -120,17 +96,59 @@ io.sockets.on('connection', function (socket) {
 	socket.on('upEvent', function (keyCode) {
 		console.log('Up');
 
-		sendData(dataStop(), myBtPort);
+		sendData(dataStop());
 	});
 
-	// listen for new serial data:  
-	myPort.on('data', function (data) {
-		console.log("Data: " + data);
-		// Convert the string into a JSON object:
-		// var serialData = JSON.parse(data);
-		// for debugging, you should see this in the terminal window:
-		// console.log(data);
-		// send a serial event to the web client with the data:
-		// socket.emit('serialEvent', serialData);
+	socket.on('listUSBPorts', function () {
+		serialport.list(function (err, ports) {
+			var output_ports = [];
+			
+			ports.forEach(function(port) {
+				console.log(port.comName + ' -- ' + port.pnpId);
+				output_ports.push(port.comName);
+			});
+
+			console.log(output_ports);
+			socket.emit('usbPortsListed', output_ports);
+		});
+	});
+
+	socket.on('usbConnect', function(port) {
+		console.log('Connecting...');
+
+		/* TODO: Handle exceptions */
+
+		myPort = new SerialPort(port, {
+			baudrate: 115200,
+			parser: serialport.parsers.readline("\r\n")
+		});
+
+		portType = 'USB';
+
+		console.log('USB connected. ' + myPort);
+	});
+
+	myBTPort.on('found', function (address, name) {
+		console.log('Bluetooth device found: ' + name + ' (' + address + ')');
+
+		socket.emit('bluetoothDeviceFound', {address: address, name: name});
+	});
+
+	socket.on('listBluetoothPorts', function () {
+		console.log("Searching for Bluetooth devices...");
+
+		myBTPort.inquire();
+	});
+
+	socket.on('bluetoothConnect', function(address) {
+		portType = 'Bluetooth';
+
+		console.log('Connecting to Bluetooth...');
+
+		myBTPort.findSerialPortChannel(address, function(channel) {
+			myBTPort.connect(address, channel, function() {
+				console.log('Bluetooth connected... ' + myBTPort);
+			});
+		});
 	});
 });
